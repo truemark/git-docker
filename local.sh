77fb2432824d500fc211866807001fca37c9a25b
Erik Jensen <ejensen@truemark.io>
2022-11-18 19:53:22 -0700

    Updating to grab the latest git and git-crypt

--- local.sh
+++ local.sh
@@ -6,8 +6,10 @@
 
 set -euo pipefail
 
-GIT_VERSION="2.34.1"
-GIT_CRYPT_VERSION="0.6.0"
+export GIT_VERSION=$(curl -sSL https://api.github.com/repos/git/git/tags | jq -r "[.[].name | select(contains(\"-rc\") | not)] | .[0]")
+echo "Using git version ${GIT_VERSION}"
+export GIT_CRYPT_VERSION=$(curl -sSL https://api.github.com/repos/AGWA/git-crypt/tags | jq -r "[.[].name | select(contains(\"debian\") | not)] | .[0]")
+echo "Using git-crypt version ${GIT_CRYPT_VERSION}"
 
 docker buildx build \
   --builder beta \
